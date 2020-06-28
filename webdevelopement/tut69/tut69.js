f8f2d76d895e1179b4b9ae5f8917bec52e82858e
harshfmdj <2017kucp1043@iiitkota.ac.in>
2020-06-28 18:46:35 +0530

    intial commit

---
+++ webdevelopement/tut69/tut69.js
@@ -0,0 +1,13 @@
+console.log("Hello its Harsh")
+// let a = setInterval(updateTime, 1000);
+// function updateTime() {
+//     console.log("Hello")
+// }
+// console.log(a);
+let b = setTimeout(() => {
+    console.log("hello");
+
+}, 5000);
+//1.0.1
+//1.1.0
+//2.0.0
